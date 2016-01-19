a295aeec2d971841670e1c772c4bc5c68c67f99d
Ben Neely <benjaminneely@u.boisestate.edu>
2016-01-19 15:09:32 -0700

    Checkpoint 04 - Requests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,9 @@ class BooksController < BlocWorks::Controller
   def index
     render :index, books: Book.all
   end
+
+  def show
+    book = Book.find(params['id'])
+    render :show, book: book
+  end
 end
